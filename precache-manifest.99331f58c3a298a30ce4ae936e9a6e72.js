4c7fe66677e9fc3eab613519402ee29d930deac1
Amna <amna.ahmad.ch@gmail.com>
2020-08-05 15:53:56 +0500

    Updates

---
+++ precache-manifest.99331f58c3a298a30ce4ae936e9a6e72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ecc1d1648c77e9c327a935a7e69f2a1",
+    "url": "/HomeFinesse/index.html"
+  },
+  {
+    "revision": "b676669bacfea0a1cac8",
+    "url": "/HomeFinesse/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "af3a95e21bf69dab7452",
+    "url": "/HomeFinesse/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b676669bacfea0a1cac8",
+    "url": "/HomeFinesse/static/js/2.8ad5e2e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HomeFinesse/static/js/2.8ad5e2e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af3a95e21bf69dab7452",
+    "url": "/HomeFinesse/static/js/main.eeeac9e0.chunk.js"
+  },
+  {
+    "revision": "3689f5e8fc0566c2e626",
+    "url": "/HomeFinesse/static/js/runtime-main.e0c2eddc.js"
+  }
+]);
